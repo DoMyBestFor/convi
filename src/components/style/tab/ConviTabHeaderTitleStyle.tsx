a0829090b5264dc64bbd2249ac0933c83658b373
jongdae_park <jongdae_park@tmax.co.kr>
2022-10-13 19:07:30 +0900

    [FIX] Move Tab Component

--- src/components/style/tab/ConviTabHeaderTitleStyle.tsx
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-import tw from 'twin.macro';
-
-export const ConviTabHeaderTitleStyle = styled.span<{ fixed: boolean | undefined }>`
-	${tw`overflow-ellipsis overflow-hidden whitespace-nowrap`}
-	${props => (props.fixed ? tw`w-full` : tw`w-3/4`)}
-`;
---
+++ src/style/tab/ConviTabHeaderTitleStyle.tsx
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+import tw from 'twin.macro';
+
+export const ConviTabHeaderTitleStyle = styled.span`
+	${tw`w-full overflow-ellipsis overflow-hidden whitespace-nowrap`}
+`;
