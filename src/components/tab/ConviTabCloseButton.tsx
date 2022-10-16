154a944c4e20e8c6260e28fe2faeede14e04d653
Park Jongdae <asdfg9377@naver.com>
2022-10-16 10:50:21 +0900

    [PRETTIER] separate button component

---
+++ src/components/tab/ConviTabCloseButton.tsx
@@ -0,0 +1,7 @@
+import { AiOutlineClose } from 'react-icons/ai';
+import styled from 'styled-components';
+import tw from 'twin.macro';
+
+export const ConviTabCloseButton = styled(AiOutlineClose)`
+	${tw`ml-auto mb-auto mt-auto hover:text-red-500`}
+`;
---
+++ src/components/tab/ConviTabPlusButton.tsx
@@ -0,0 +1,7 @@
+import { AiOutlinePlus } from 'react-icons/ai';
+import styled from 'styled-components';
+import tw from 'twin.macro';
+
+export const ConviTabPlusButton = styled(AiOutlinePlus)`
+	${tw`mt-auto mb-1 cursor-pointer hover:text-gray-500`}
+`;
