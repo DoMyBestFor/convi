343a782fac1f254f54de30b9da737c042f88797b
jongdae_park <jongdae_park@tmax.co.kr>
2022-10-17 19:17:25 +0900

    [ADD] index.tsx remove, index.ts add

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { ConviModal } from './lib/modal/ConviModal';
+import { ConviTab } from './lib/tab/ConviTab';
+import { ConviTabElement } from './lib/tab/ConviTabElement';
+
+export { ConviTab, ConviTabElement, ConviModal };
+
+export default {
+	ConviTab,
+	ConviTabElement,
+	ConviModal,
+};
