cc217435b11bdec863d9ef37a957590f9d6b2b82
Park Jongdae <asdfg9377@naver.com>
2022-10-23 17:27:10 +0900

    [FIX] enable to add one tab elements in uncontrolled tab

--- src/lib/tab/ConviUncontrolledTab.tsx
+++ src/lib/tab/ConviUncontrolledTab.tsx
@@ -9,36 +9,40 @@ import { ConviTabStyle } from '../style/ConviTabStyle';
 export interface ConviUncontrolledTabProps {
 	defaultIndex: number;
 	forceRender?: boolean;
-	children: React.ReactElement<ConviTabElementProps>[];
+	children: React.ReactElement<ConviTabElementProps> | React.ReactElement<ConviTabElementProps>[];
 }
 
 export const ConviUncontrolledTab: React.FC<ConviUncontrolledTabProps> = props => {
 	const { defaultIndex, forceRender, children } = props;
 	const [selected, setSelected] = useState<number>(defaultIndex);
 
+	const tabElements: React.ReactElement<ConviTabElementProps>[] = Array.isArray(children) ? children : [children];
+
 	return (
 		<ConviTabStyle>
-			<div>
-				{children.map((child: React.ReactElement<ConviTabElementProps>, tabIndex: number) => (
-					<ConviTabHeaderElement
-						key={`${child.props.title}-${tabIndex * 1}`}
-						index={tabIndex}
-						selected={selected === tabIndex}
-						icon={child.props.frontIcon}
-						onTabTitleChange={() => null}
-						fixed={child.props.fixed || defaultIndex !== undefined}
-						onSelected={(index: number) => setSelected(index)}
-						onClose={() => null}
-						onHeaderDrag={(e: any) => console.log(e)}
-						onHeaderDragEnd={(e: any) => console.log(e)}
-					>
-						{child.props.title}
-					</ConviTabHeaderElement>
-				))}
+			<div className="header">
+				<div className="tabList">
+					{tabElements.map((child: React.ReactElement<ConviTabElementProps>, tabIndex: number) => (
+						<ConviTabHeaderElement
+							key={`${child.props.title}-${tabIndex * 1}`}
+							index={tabIndex}
+							selected={selected === tabIndex}
+							icon={child.props.frontIcon}
+							onTabTitleChange={() => null}
+							fixed={child.props.fixed || defaultIndex !== undefined}
+							onSelected={(index: number) => setSelected(index)}
+							onClose={() => null}
+							onHeaderDrag={(e: any) => console.log(e)}
+							onHeaderDragEnd={(e: any) => console.log(e)}
+						>
+							{child.props.title}
+						</ConviTabHeaderElement>
+					))}
+				</div>
 			</div>
 
 			{!forceRender
-				? children.map((child, index) => (
+				? tabElements.map((child, index) => (
 						<span
 							key={`${child.props.title}-${index * 1}`}
 							css={css`
@@ -48,7 +52,7 @@ export const ConviUncontrolledTab: React.FC<ConviUncontrolledTabProps> = props =
 							{child}
 						</span>
 				  ))
-				: children[selected]}
+				: tabElements[selected]}
 		</ConviTabStyle>
 	);
 };
