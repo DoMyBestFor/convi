6479b063051632d3e31adfdec89c63d63b19b9c2
Park Jongdae <asdfg9377@naver.com>
2022-11-13 23:29:48 +0900

    [FIX] convi style 공통화 작업 - color theme 추가

---
+++ src/style/global/theme.ts
@@ -0,0 +1,33 @@
+export const theme = Object.freeze({
+	colors: {
+		// white / black
+		white: '#FFFFFF',
+		black: '#000000',
+
+		// primary
+		primary: '#2979FF',
+		'primary-hover': '#70A5FF',
+		'primary-active': '#F6FAFC',
+
+		// secondary
+		secondary: '#FAFAFA',
+		'secondary-hover': '#F2F2F2',
+		'secondary-active': '#ECECEC',
+
+		// info
+		info: '#0EB81F',
+		'info-hover': '#1BD689',
+		'info-active': '#E5FFE4',
+
+		// error
+		error: '#F10000',
+		'error-hover': '#FF4768',
+		'error-active': '#FBE8EB',
+
+		// warning
+		warning: '#FFF8E9',
+
+		// violet
+		violet: '#98109B',
+	},
+});
