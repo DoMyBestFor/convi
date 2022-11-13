b4c1d1b212b785bebf93cc44fea2abe02305c813
Park Jongdae <asdfg9377@naver.com>
2022-11-13 23:59:11 +0900

    [ADD] font size style

---
+++ src/style/global/font.ts
@@ -0,0 +1,51 @@
+// font size
+export const font = Object.freeze({
+	h1: `
+        font-weight: bold;
+        font-size: 30px;
+        transform: sentence;
+        letter-spacing: 0.25px;
+    `,
+	h2: `
+        font-weight: regular;
+        font-size: 24px;
+        transform: sentence;
+        letter-spacing: 0.15px;
+    `,
+	h3: `
+        font-weight: semibold;
+        font-size: 18px;
+        transform: sentence;
+        letter-spacing: 0.15px;
+    `,
+	h4: `
+        font-weight: semibold;
+        font-size: 13px;
+        transform: sentence;
+        letter-spacing: 0.15px;
+    `,
+	body1: `
+        font-weight: medium;
+        font-size: 12px;
+        transform: sentence,
+        letter-spacing: 0.15px;
+    `,
+	body2: `
+        font-weight: regular;
+        font-size: 11px;
+        transform: sentence,
+        letter-spacing: 0.15px;
+    `,
+	body3: `
+        font-weight: regular;
+        font-size: 10px;
+        transform: sentence,
+        letter-spacing: 0.15px;
+    `,
+	body4: `
+        font-weight: medium;
+        font-size: 9px;
+        transform: sentence,
+        letter-spacing: 0.15px;
+    `,
+});
