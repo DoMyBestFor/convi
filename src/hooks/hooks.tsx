5c6336d8c9e158eb43be5fa5b09956eea7f1da72
Park Jongdae <asdfg9377@naver.com>
2022-12-01 23:55:15 +0900

    [FIX] hooks 분리

---
+++ src/hooks/hooks.tsx
@@ -0,0 +1,40 @@
+/** @jsxImportSource @emotion/react */
+import React from 'react';
+import { useState, useEffect } from 'react';
+
+export const useTabTitles = (title: string, onTabTitleChange: (newTitle: string) => void, ableChangeTitle: boolean) => {
+	const [editMode, setEditMode] = useState<boolean>(false);
+	const [tabTitle, setTabTitle] = useState<string>(title);
+
+	useEffect(() => {
+		setTabTitle(title);
+	}, [title]);
+
+	const handleDoubleClick = () => setEditMode(true);
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => setTabTitle(e.target.value);
+
+	return {
+		titleContent: () =>
+			editMode && ableChangeTitle ? (
+				<input
+					css={editStyle}
+					value={tabTitle}
+					onChange={handleChange}
+					onBlur={e => {
+						onTabTitleChange(e.target.value);
+						setEditMode(false);
+					}}
+					onKeyDown={e => {
+						if (e.key === 'Enter') {
+							onTabTitleChange(e.currentTarget.value);
+							setEditMode(false);
+						}
+					}}
+				/>
+			) : (
+				<span css={nonEditStyle} onDoubleClick={handleDoubleClick}>
+					{tabTitle}
+				</span>
+			),
+	};
+};
